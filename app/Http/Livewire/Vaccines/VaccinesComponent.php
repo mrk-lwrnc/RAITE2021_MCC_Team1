06f5430fb2be9e66d1ea5f49f24ed5a86bfb1aa5
dexcabigting <dexcabigting1234@gmail.com>
2021-11-06 11:52:13 +0800

    Added VaccinesComponent

---
+++ app/Http/Livewire/Vaccines/VaccinesComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Vaccines;
+
+use Livewire\Component;
+
+class VaccinesComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.vaccines.vaccines-component');
+    }
+}
---
+++ app/Models/Vaccine.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vaccine extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'vaccine_name',
+        'vaccine_info',
+        'vaccine_restriction',
+    ];
+}
