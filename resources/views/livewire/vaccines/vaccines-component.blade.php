bf98221ee9b1d585964c6bc762fbedc6a3435615
dexcabigting <dexcabigting1234@gmail.com>
2021-11-06 12:20:35 +0800

    addVaccine now working

--- resources/views/livewire/vaccines/vaccines-component.blade.php
+++ resources/views/livewire/vaccines/vaccines-component.blade.php
@@ -11,24 +11,30 @@
                 <div class="flex flex-col gap-5">
                     Add Vaccine
                     <form wire:submit.prevent="addVaccine">
+                        @if(session()->has('success'))
+                        <div>
+                            {{ session('success') }}
+                        </div>
+                        @endif
+
                         <div>
                             <label>Vaccine Name</label>
-                            <input type="text" wire:model="form.vaccine_name" required>
+                            <input type="text" wire:model.defer="form.vaccine_name" required>
                         </div>
 
                         <div>
                             <label>Vaccine Manufacturer</label>
-                            <input type="text" wire:model="form.vaccine_manufacturer" required>
+                            <input type="text" wire:model.defer="form.vaccine_manufacturer" required>
                         </div>
 
                         <div>
                             <label>Vaccine Info</label>
-                            <input type="text" wire:model="form.vaccine_info" required>
+                            <input type="text" wire:model.defer="form.vaccine_info" required>
                         </div>
 
                         <div>
                             <label>Vaccine Restriction</label>
-                            <input type="text" wire:model="form.vaccine_restriction" required>
+                            <input type="text" wire:model.defer="form.vaccine_restriction" required>
                         </div>
 
                         <div>
